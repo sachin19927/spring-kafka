25bc7a4463a26ed43a7cf6dc261803f4f2541ef2
SACHIN HS <sachin19927@gmail.com>
2026-07-08 00:45:07 +0530

    Kafka Improvised

---
+++ spring-kafka-parent/shared-events/src/main/java/com/spring/poc/kafka/model/OrderRequest.java
@@ -0,0 +1,8 @@
+package com.spring.poc.kafka.model;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(String customerId,
+                           String productId, Integer quantity,
+                           BigDecimal price) {
+}
